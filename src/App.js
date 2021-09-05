c8e0d66ee660c15475ae42c0083f6f79b7991d0d
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2021-09-05 18:45:00 +0200

    components was asses

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import React,{Comment} from 'react'
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className="app-wrapper">
 
-          <header className = "header">
-          <img src = "https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg" />
-          </header> 
-          <nav className="nav">
-            <div> <a> Profile </a> </div>
-            <div> <a> Messsages </a> </div>
-            <div> <a> News </a> </div>
-            <div> <a> Musik </a> </div>
-            <div> <a> Settings </a> </div>
-          </nav>
-          <div className = "content">
-            <img src = "https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"/>
-            <div>
-            ava + descroition
-          </div>
-          <div>
-            My post
-              <div>
-                New post
-              </div>
-          </div>
-          <div>
-            <div>
-              post1
-            </div>
-            <div>
-              post2
-            </div>
-          </div>
-
-          </div>
+          <Header/>
+          <Navbar/>
+          <Profile/>
          
   
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+    return <header className = "header">
+          <img src = "https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg" />
+        </header> 
+
+
+  
+
+}
+
+export default Header;
