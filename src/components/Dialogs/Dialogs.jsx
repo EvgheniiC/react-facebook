b421bcd8c23f415225c375cd7bd4c4a78cebc25c
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2021-09-11 09:50:11 +0200

    NavLink and Dialogs was changed

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
 
-    return  <div>
-              Dialogs
+    return  <div className = {classes.dialogs}>
+              <div className = {classes.dialogsItems}>
+                  <div className = {classes.dialog + " " + classes.active}>
+                    Zeka
+                  </div>
+                  <div className = {classes.dialog}>
+                    Vasia
+                  </div>
+                  <div className = {classes.dialog}>
+                    Petia
+                  </div>
+                  <div className = {classes.dialog}>
+                    Valera
+                  </div>
+                  <div className = {classes.dialog}>
+                    Cosc
+                  </div>
+                  <div className = {classes.dialog}>
+                    Free
+                  </div>
+              </div>
+              <div className = {classes.messages}>
+                <div className = {classes.message}>Hallo</div>
+                <div className = {classes.message}>React</div>
+                <div className = {classes.message}>Bey</div>
+              </div>
             </div>
 
 }
