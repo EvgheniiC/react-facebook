5581c1a08667834bfd9b8dbb8c23637fd353db49
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2021-09-24 06:40:00 +0200

    update App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
 import React,{Comment} from 'react'
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import {BrowserRouter,Route} from 'react-router-dom' 
+import {Route} from 'react-router-dom' 
 import Musik from './components/Musik/Musik';
 import News from './components/News/News';
 import Settingses from './components/Settingses/Settingses';
@@ -13,8 +12,7 @@ import Friends from './components/Friends/Friends';
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
+          <div className="app-wrapper">
           <Header/>
           <Navbar state = {props.state.sidebar}/>
           <div className = "app-wrapper-content">
@@ -28,7 +26,6 @@ const App = (props) => {
             <Route path = '/friends' render = {() => <Friends/>}/>
           </div>
       </div>
-    </BrowserRouter>
   );
 }
 
