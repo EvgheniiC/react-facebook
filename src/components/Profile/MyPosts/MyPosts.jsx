c0de5c26676c0df4c8d1d9249fb101708dc8518b
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2021-12-01 06:06:03 +0100

    Bug fixed

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -15,7 +15,7 @@ const MyPosts = (props) => {
 
   let onPostChange = () => {
     let text = newPostElement.current.value
-    props.updateNewPostChange(text)
+    props.updateNewPostText(text)
   }
     
     return  <div className = {classes.postBlock}>
