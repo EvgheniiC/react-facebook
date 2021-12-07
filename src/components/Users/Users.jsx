00a9f99d3a053c21e5d8186afbb1527df9175315
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2021-12-07 06:40:26 +0100

    REST API was added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import styles from "./Users.module.css";
+import * as axios from "axios"
+import userPhoto from "../../../src/assets/img/man.png"
 
 const Users = (props) => {
-
+  
   if (props.users.lenght === 0){
-  props.setUsers(
-    [
-      {id: 1, photoURL : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIAwu6j26w8cXfrJaFyiFkI3syKCc5Z0HpDw&usqp=CAU",followed : false, fullName: "Evghenii", status : "I am hier", location :{city : 'Bremen', country: 'DE'}},
-      {id: 2, photoURL : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIAwu6j26w8cXfrJaFyiFkI3syKCc5Z0HpDw&usqp=CAU", followed : true, fullName: "Alex", status : "I am hier", location :{city : 'Moskow', country: 'RU'}},
-      {id: 3, photoURL : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIAwu6j26w8cXfrJaFyiFkI3syKCc5Z0HpDw&usqp=CAU", followed : false, fullName: "Vladimir", status : "I am hier", location :{city : 'Tiraspol', country: 'MD'}},
-      {id: 4, photoURL : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIAwu6j26w8cXfrJaFyiFkI3syKCc5Z0HpDw&usqp=CAU", followed : false, fullName: "Martin", status : "I am hier", location :{city : 'Rostock', country: 'DE'}}
-    ]
-  )
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      debugger
+      props.setUsers(response.data.items)
+    })
   }
+ 
 
     return  <div>
               {
                 props.users.map(u => <div key = {u.id}>
                   <span>
                     <div>
-                      <img src= {u.photoURL} className = {styles.userPhoto}/>
+                      <img src= {u.photos.small !== null ? u.photos.small : userPhoto} className = {styles.userPhoto}/>
                     </div>
                     <div>
                       {u.followed ?
@@ -30,12 +29,12 @@ const Users = (props) => {
                   </span> 
                   <span>
                     <span>
-                      <div>{u.fullName}</div>
+                      <div>{u.name}</div>
                       <div> {u.status}</div>
                     </span>
                     <span>
-                      <div>{u.location.country}</div>
-                      <div>{u.location.city}</div>
+                      <div>{"u.location.country"}</div>
+                      <div>{"u.location.city"}</div>
                     </span>
                   </span>              
                 </div>)
