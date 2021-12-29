0b03e9daac188a2b9959dbec423fe9e2133711c3
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2021-12-29 14:47:09 +0100

    code review

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -8,11 +8,14 @@ const ProfileInfo = (props) => {
     let userId = props.match.params.userId;
 
     function findArrayElementById(array, id) {
+      if (!id){
+        id = 21242
+      }
         return array.find((element) => {
           return element.id == id;
         })
       }
-        
+    
     if(!props.profile){
         return <Preloader/>
     }
@@ -21,7 +24,7 @@ const ProfileInfo = (props) => {
                 <img src = "https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"/>
                 <div className = {classes.descriptionBlock}>
                     <img src={findArrayElementById(props.profile.items,userId).photos.small} height={150} />
-                    <div> {props.profile.items[1].status}</div>
+                    <div> {findArrayElementById(props.profile.items,userId).status}</div>
                 </div>
             </div>
 }
