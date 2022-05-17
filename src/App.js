bc4d79776cc6d70cdbb1c54a3176d6b7b91d6fb1
ECostin84 <evghenii.costin@sixt.com>
2022-05-17 11:39:56 +0200

    94 - React.lazy и React.Suspense

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Musik from './components/Musik/Musik';
 import News from './components/News/News';
 import Settingses from './components/Settingses/Settingses';
 import Friends from './components/Friends/Friends';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from './components/Login/Login';
 import { initializeApp } from "../../react-facebook/src/redux/appReducer";
@@ -18,6 +16,12 @@ import Preloader from './components/common/Preloader/Preloader';
 import {Provider} from "react-redux";
 import { BrowserRouter } from 'react-router-dom'; 
 import store from './redux/reduxStore';
+
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
+// import ProfileContainer from './components/Profile/ProfileContainer';
+
+const DialogsContainer = React.lazy(() => import ('./components/Dialogs/DialogsContainer'))
+const ProfileContainer = React.lazy(() => import ('./components/Profile/ProfileContainer'))
 
 class App extends Component {
 
@@ -34,6 +38,7 @@ class App extends Component {
           <HeaderContainer/>
           <Navbar />
           <div className = "app-wrapper-content">
+          <React.Suspense fallback={<Preloader/>}>
             <Route path = '/dialogs' render = {() =>
              <DialogsContainer />
             }
@@ -48,6 +53,7 @@ class App extends Component {
             <Route path = '/users' render = {() => <UsersContainer/>}/>
             <Route path = '/login' render = {() => <LoginPage/>}/>
             <Route path = '/friends' render = {() => <Friends/>}/>
+            </React.Suspense>
           </div>
       </div>
   );
